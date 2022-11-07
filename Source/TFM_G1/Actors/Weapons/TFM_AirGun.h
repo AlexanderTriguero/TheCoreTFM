d7f83be792bfd5f8e68c1088bbbaff69f2020d5b
AlexTriguero <alex.triguero14@gmail.com>
2022-11-07 11:18:35 +0100

    +AirGun

---
+++ Source/TFM_G1/Actors/Weapons/TFM_AirGun.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Weapons/TFM_WeaponBase.h"
+#include "TFM_AirGun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TFM_G1_API ATFM_AirGun : public ATFM_WeaponBase
+{
+	GENERATED_BODY()
+	
+};
