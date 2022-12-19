8bbdb2d4adf0235f13e3b452afd03532e29bf62f
FFola <franco.folatelli@hotmail.com>
2022-12-19 17:59:26 +0100

    -Particle Effects

---
+++ Source/TFM_G1/Actors/Bubbles/TFM_BubbleElectric.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Bubbles/TFM_BubbleBase.h"
+#include "TFM_BubbleElectric.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TFM_G1_API ATFM_BubbleElectric : public ATFM_BubbleBase
+{
+	GENERATED_BODY()
+	
+};
