985c80ab2b8fd1c52356c43c0e2055b3427fea4d
AlexTriguero <alex.triguero14@gmail.com>
2023-01-13 18:15:10 +0100

    Nivel Hub_Lab + Potales

--- Source/TFM_G1/Actors/LevelObjects/TFM_EndOfLevel.h
+++ Source/TFM_G1/Actors/LevelObjects/TFM_EndOfLevel.h
@@ -20,12 +20,12 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "End Of Level")
 	FName LevelToLoad;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "End Of Level")
+		UBoxComponent* BoxCollider = nullptr;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "End Of Level")
-		UBoxComponent* BoxCollider = nullptr;
 
 	UFUNCTION()
 		void OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
